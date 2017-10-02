02880a6b9324747249158b61aa254c7355ecebbe
marat_enikeev <mat-83@mail.ru>
2017-10-02 18:32:08 +0500

    Добавляет словарь для ключей и мелкие фиксы

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import lasio
+import lasio # http://pythonhosted.org/lasio/
 import pandas
 import work_with_dir
 import csv
-keys_tuple = ("well_name", "start_m", "stop_m")
+
+# Сохранение Las файлов в виде csv, для дальнейшей обработки в pandas
+
+kid_well = 0
+kid_start = 1
+kid_end = 2
+
+keys_dict = {kid_well:"well_name", kid_start:"start_m", kid_end:"stop_m"}
 
 main_dir = "D:\\NIPI\\machine learning\\tasks\\task 6\\Data\\las"
 format = ".LAS"
 res_files = work_with_dir.GetFilesInDir(main_dir, format)
-file = open(main_dir + "\\" + "example.csv", "w", newline="")
-for file_name in res_files:
-    dict_list = []
-    # инициализируем словарь
-    d = dict.fromkeys(keys_tuple)
-    dict_list.append(d)
-    l = lasio.read(main_dir + "\\" + file_name)
-    print("In file" + file_name)
-    # получить список кривых и их описание
-    #for curve in l.curves:
-    #    print("%s\t[%s]\t%s\t%s" % (
-    #    curve.mnemonic, curve.unit, curve.value, curve.descr))
-    d["well_name"] = file_name[:-4]
-    d["start_m"] = l.well["STRT"].value
-    d["stop_m"] = l.well["STOP"].value
 
-    keys = d.keys()
-    print(d.keys())
-    dict_writer = csv.DictWriter(file, keys_tuple, delimiter = ";")
-    dict_writer.writeheader()
+
+file = open(main_dir + "\\" + "example.csv", "w", newline="")
+keys = keys_dict.values()
+
+dict_writer = csv.DictWriter(file, keys, delimiter = ";")
+dict_writer.writeheader()
+
+
+for file_name in res_files:
+    print("From file" + file_name)
+
+    dict_list = []
+    # инициализируем словарь и заполняем в соответсви с ласом
+    d = dict.fromkeys(keys)
+    l = lasio.read(main_dir + "\\" + file_name)
+    d[keys_dict[kid_well]] = file_name[:-4]
+    d[keys_dict[kid_start]] = l.well["STRT"].value
+    d[keys_dict[kid_end]] = l.well["STOP"].value
+
+    dict_list.append(d)
     dict_writer.writerows(dict_list)
-    print("\n\n")
-    break
+
+
+print("end save csv")
 file.close()
 
 
+# получить список кривых и их описание
+# for curve in l.curves:
+#    print("%s\t[%s]\t%s\t%s" % (
+#    curve.mnemonic, curve.unit, curve.value, curve.descr))
