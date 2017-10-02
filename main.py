2907cb20896fe4e939f96092f436e40766a09466
marat_enikeev <mat-83@mail.ru>
2017-10-03 01:45:05 +0500

    Добвылет новые ключи

--- main.py
+++ main.py
@@ -6,8 +6,31 @@ import csv
 # Сохранение Las файлов в виде csv, для дальнейшей обработки в pandas
 
 kid_well = 0
-kid_start = 1
-kid_end = 2
+kid_start = kid_well + 1
+kid_end = kid_start + 1
+kid_depth = kid_end + 1
+kid_aps = kid_depth + 1
+kid_rp = kid_aps + 1
+kid_kp = kid_rp + 1
+kid_kgl = kid_kp + 1
+kid_kpr = kid_kgl + 1
+kid_kvo = kid_kpr + 1
+kid_kng = kid_kvo + 1
+kid_lit = kid_kng + 1
+kid_sat = kid_lit + 1
+
+
+
+#kid_depth, "DEPT.M"
+#kid_aps, "aps.US",
+#kid_rp, "rp.Ohm.M",
+#kid_kp, "kp.%",
+#kid_kgl, "kgl.%",
+#kid_kpr, "kpr.mD",
+#kid_kvo, "kvo.%",
+#kid_kng, "kng.%",
+#kid_lit, "lit.US",
+#kid_sat, "satur.US",
 
 keys_dict = {kid_well:"well_name", kid_start:"start_m", kid_end:"stop_m"}
 
@@ -37,7 +60,12 @@ for file_name in res_files:
     dict_list.append(d)
     dict_writer.writerows(dict_list)
 
+    # получить список кривых и их описание
+    for curve in l.curves:
+        print("%s\t[%s]\t%s\t%s" % (
+        curve.mnemonic, curve.unit, curve.value, curve.descr))
 
+    break
 print("end save csv")
 file.close()
 
