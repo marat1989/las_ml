5a6d3926f495cb2034e2153011c82a8ac5f86015
Miller <fazlitdin_marat@mail.ru>
2017-10-03 13:16:23 +0600

    Рефакторинг

--- main.py
+++ main.py
@@ -28,25 +28,28 @@ kid_lit = kid_kng + 1
 kid_sat = kid_lit + 1
 
 
-keys_dict = {kid_well:"well_name", kid_start:"start_m", kid_end:"stop_m", kid_depth: "DEPT", kid_aps: "aps",
+keys_dict = {kid_well:"well_name", kid_start:"STRT", kid_end:"STOP", kid_depth: "DEPT", kid_aps: "aps",
             kid_rp: "rp", kid_kp: "kp", kid_kgl: "kgl", kid_kpr: "kpr", kid_kvo: "kvo", kid_kng: "kng", kid_lit: "lit",
             kid_sat: "satur"}
 
-main_dir = "..\\tasks\\task 6\\Data\\las"
-format = ".LAS"
-res_files = work_with_dir.GetFilesInDir(main_dir, format)
-file = open(main_dir + "\\" + "example.csv", "w", newline="")
-keys = keys_dict.values()
+data_dir = "..\\tasks\\task 6\\Data"
+las_dir = data_dir + "\\las"
+csv_out_file = data_dir + "\\las_data.csv"
+ext_format = ".LAS"
 
-dict_writer = csv.DictWriter(file, keys, delimiter = ";")
+las_files = work_with_dir.GetFilesInDir(las_dir, ext_format)
+
+keys_list = keys_dict.values()
+csv_out_stream = open(csv_out_file, "w", newline="")
+dict_writer = csv.DictWriter(csv_out_stream, keys_list, delimiter=";")
 dict_writer.writeheader()
 
-for file_name in res_files:
-    print("From file" + file_name)
+for las_file_name in las_files:
+    print("From file" + las_file_name)
 
     dict_list = []
     # инициализируем словарь и заполняем в соответсви с ласом
-    l = lasio.read(main_dir + "\\" + file_name)
+    l = lasio.read(las_dir + "\\" + las_file_name)
     # получить список кривых и их описание
     n = int(len(l["DEPT"]))
     print(n)
@@ -56,10 +59,10 @@ for file_name in res_files:
         # print("%s\t[%s]\t%s\t%s" % (
         # curve.mnemonic, curve.unit, curve.value, curve.descr))
         #for curve in l.curves:
-        d = dict.fromkeys(keys)
-        d[keys_dict[kid_well]] = file_name[:-4]
-        d[keys_dict[kid_start]] = l.well["STRT"].value
-        d[keys_dict[kid_end]] = l.well["STOP"].value
+        d = dict.fromkeys(keys_list)
+        d[keys_dict[kid_well]] = las_file_name[:-4]
+        d[keys_dict[kid_start]] = l.well[keys_dict[kid_start]].value
+        d[keys_dict[kid_end]] = l.well[keys_dict[kid_end]].value
 
         d[keys_dict[kid_depth]] = CheckIsNan(l[keys_dict[kid_depth]][i], default)
         d[keys_dict[kid_aps]] = CheckIsNan(l[keys_dict[kid_aps]][i], default)
@@ -76,8 +79,13 @@ for file_name in res_files:
     break
 
 print("end save csv")
-file.close()
+csv_out_stream.close()
 
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv(csv_out_file)
+print(df.head())
 
 # получить список кривых и их описание
 # for curve in l.curves:
